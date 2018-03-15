c5a144091cdad08965ce2357dafb2fe11ab900e6
AndreaPAbundis <lizbethabundispgmail.com>
2018-03-15 16:46:29 -0600

    WIP-Andrea
    componentes y admin

---
+++ public_html/components/Componente1.inc.php
@@ -0,0 +1,14 @@
+<div id="Componente1" class="container-comp1">
+  <div class="img-container">
+    <img src="https://dummyimage.com/500x300/000/fff"></img>
+  </div>
+  <div class="description">
+    <div class="title-text">
+      <h1><?php echo $data['title'] ?></h1>
+      <p><?php echo $data['subtitle'] ?></p>
+    </div>
+    <div class="button-content">
+        <a href="<?php echo $data['btn_link'] ?>"><button><?php echo $data['btn_title']; ?></button></a>
+    </div>
+  </div>
+</div>
---
+++ public_html/components/Componente4.inc.php
@@ -0,0 +1,13 @@
+<div>
+	<div class="content-cita">
+		<div class="content-picture">
+			<img src="https://cambiardeimagen.files.wordpress.com/2013/03/moda-masculina-lentes-cara-hombre-carametria-caramorfoligia-consultoria-de-imagen.jpg?w=646&h=823">
+		</div>
+		<div class="content-info">
+			<h1><?php echo $data['title'] ?></h1>
+			<p><strong><em><?php echo $data['subtitle'] ?></em></strong></p>
+			<p>fulanito de tal</p>
+			   <a href="<?php echo $data['btn_link'] ?>"><button><?php echo $data['btn_title']; ?></button></a>
+		</div>
+	</div>
+</div>
